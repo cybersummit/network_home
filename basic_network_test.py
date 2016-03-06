fa4737566582501ec618248259a436f2a8acdc67
cybersummit <Rich@computeitnow.com>
2016-03-06 16:03:56 -0700

    Added Network Test File

--- basic_network_test.py
+++ basic_network_test.py
@@ -1,11 +1,12 @@
 # Network-TEst.py
-# import socket
-# import time
-# import telnetlib
-# # get local machine name
-# host = socket.gethostname()
-# print host
-# ip = socket.getaddrinfo ()
+import socket
+import time
+import telnetlib
+get local machine name
+host = socket.gethostname()
+print host
+#TODO Fix getaddrinfo syntax
+#ip = socket.getaddrinfo ()
 # print ip
 import logging
 
